8f29f9f52133fcd52f26dd9f4eee56feee753434
Albert <albertcaprar@gmail.com>
2023-11-17 10:35:49 +0200

    modified files so routing works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent} from "./login/login.component";
+import { LandingPageComponent} from "./landing-page/landing-page.component";
+import { WorkoutsComponent} from "./workouts/workouts.component";
+import { ExercisesComponent} from "./exercises/exercises.component";
+import { FoodComponent} from "./food/food.component";
+import { FoodTrackerComponent} from "./food-tracker/food-tracker.component";
+import { CurrentWorkoutComponent} from "./current-workout/current-workout.component";
+import { ProfileComponent} from "./profile/profile.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'landing-page', component:LandingPageComponent },
+  {path: 'workouts', component: WorkoutsComponent},
+  {path: 'current-workout', component: CurrentWorkoutComponent},
+  {path: 'exercises', component: ExercisesComponent},
+  {path: 'food', component: FoodComponent},
+  {path: 'food-tracker', component: FoodTrackerComponent},
+  {path: '**', redirectTo: 'landing-page'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
