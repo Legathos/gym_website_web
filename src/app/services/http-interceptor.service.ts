c62ba25701da5b4162f4c81ac4db74d3c19c47a5
Albert <albertcaprar@gmail.com>
2023-12-16 16:10:36 +0200

    fixed cookie bug

--- src/app/services/http-interceptor.service.ts
+++ src/app/services/http-interceptor.service.ts
@@ -14,7 +14,7 @@ export class HttpInterceptorService implements HttpInterceptor {
   constructor(private cookieService: CookieService, private jwtService: JwtServiceService) { }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     if (!req.url.includes("login")) {
-      const jwt = this.cookieService.get("app-auth");
+      const jwt = this.cookieService.get("auth-cookie");
       if (jwt !== '' && Date.now() > this.jwtService.parseJwt(jwt).exp * 1000) {
         //TO DO: Afiseaza modal sau o pagina noua si dupa redirect
         return EMPTY;
