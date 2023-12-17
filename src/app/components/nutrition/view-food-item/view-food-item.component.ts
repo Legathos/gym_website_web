88bf11575b6e21d5713f82fab433896d04901f14
Albert <albertcaprar@gmail.com>
2023-12-17 12:47:06 +0200

    added a function to get a food item

--- src/app/components/nutrition/view-food-item/view-food-item.component.ts
+++ src/app/components/nutrition/view-food-item/view-food-item.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RequestsService} from "../../../services/requests.service";
+import {FoodComponent} from "../food/food.component";
+import {FoodData} from "../../../../data/food.data";
 
 @Component({
   selector: 'app-view-food-item',
   templateUrl: './view-food-item.component.html',
   styleUrl: './view-food-item.component.scss'
 })
-export class ViewFoodItemComponent {
+export class ViewFoodItemComponent implements OnInit{
+
+  foodItem!:[FoodData];
+
+  constructor(private requestsService:RequestsService) {
+  }
+
+  ngOnInit() {
+
+  }
+
+  getFoodItem(id:number){
+    this.requestsService.getFoodItemById(id).subscribe({
+      next:(data)=>{
+        this.foodItem = data;
+      }
+    })
+  }
 
 }
