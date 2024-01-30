65f0f01201f8c4f8314fb1373bd00ba6a29d6626
Albert <albertcaprar@gmail.com>
2024-01-30 12:44:04 +0200

    Updated to match with convention

--- src/app/services/food/services/food.service.ts
+++ src/app/services/food/services/food.service.ts
@@ -2,16 +2,14 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { FoodData } from '../model/food.model';
 import { EndpointDictionary } from '../../../../environments/endpoint-dictionary';
-import { HttpUtilsService } from '../../http-utils';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class FoodService {
 
-  constructor(private httpUtilsService: HttpUtilsService, private httpClient: HttpClient) { }
+  constructor( private httpClient: HttpClient) { }
 
   getAllFoodItems():Observable<any>{
-    const url = this.httpUtilsService.getFullUrl(EndpointDictionary.getAllFoodItems);
-    return this.httpClient.get<FoodData[]>(url);
+    return this.httpClient.get<FoodData[]>(EndpointDictionary.getAllFoodItems);
   }
 }
