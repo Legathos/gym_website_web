b34e5804b66e95568a3874713c46aa08df9c4d0b
denismaris <marisdenis3333@gmail.com>
2024-02-01 00:29:51 +0200

    module import fixes

--- src/app/domain/user-login/user-login.module.ts
+++ src/app/domain/user-login/user-login.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UserLoginService } from '@domain/user-login';
+import { UserLoginService } from '@domain/user-login/service/user-login.service';
 
 @NgModule({
   declarations: [],
