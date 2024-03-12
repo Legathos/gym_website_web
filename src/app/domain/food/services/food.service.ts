35c8f38e447e3f1b5784bb2234cb0de6400c0c9e
Albert <albertcaprar@gmail.com>
2024-03-12 18:47:27 +0200

    added get for food logs

--- src/app/domain/food/services/food.service.ts
+++ src/app/domain/food/services/food.service.ts
@@ -6,13 +6,21 @@ import { Observable } from 'rxjs';
 import { EndpointDictionary } from '../../../../environments/endpoint-dictionary';
 
 import { FoodData } from '@domain/food';
+import {LoggerData} from "../../../../data/logger.data";
+import {MemberService} from "@domain/member";
 
 @Injectable()
 export class FoodService {
 
-  constructor( private httpClient: HttpClient) { }
+  constructor( private httpClient: HttpClient,private memberService: MemberService) { }
 
   getAllFoodItems():Observable<any>{
     return this.httpClient.get<FoodData[]>(EndpointDictionary.getAllFoodItems);
+  }
+
+  getFoodTrackingByIdAndDate( date:String):Observable<any>{
+    let id  = this.memberService.getUserId()
+    return this.httpClient.get<LoggerData[]>(EndpointDictionary.getFoodTrackingByIdAndDate+id+`/`+date)
+
   }
 }
