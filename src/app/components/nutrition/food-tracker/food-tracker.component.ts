88773eaecb0843c973a06d38e6545aaabe5a9b97
Albert <albertcaprar@gmail.com>
2024-03-12 18:47:05 +0200

    changed most of the code from hardcoded values to proper api calls

--- src/app/components/nutrition/food-tracker/food-tracker.component.ts
+++ src/app/components/nutrition/food-tracker/food-tracker.component.ts
@@ -1,21 +1,32 @@
 import {Component, OnInit} from '@angular/core';
-import {Chart, plugins} from "chart.js";
+import {Chart} from "chart.js";
+import {FoodService} from "@domain/food";
+import {LoggerData} from "../../../../data/logger.data";
+import {MemberService} from "@domain/member";
+import {timeout} from "rxjs";
 
 @Component({
   selector: 'app-food-tracker',
   templateUrl: './food-tracker.component.html',
   styleUrl: './food-tracker.component.scss'
 })
-export class FoodTrackerComponent implements OnInit{
+export class FoodTrackerComponent implements OnInit {
 
-  protein= 150;
-  carbs=200;
+  foodLogs: LoggerData[] = [];
+  protein = 150;
+  carbs = 200;
   fats = 50;
-  calories=this.protein*4+this.carbs*4+this.fats*9;
+  calories = this.protein * 4 + this.carbs * 4 + this.fats * 9;
+
+  constructor(private foodService: FoodService) {
+  }
 
   ngOnInit(): void {
-  this.macrosChart()
+    this.macrosChart()
+    this.getFoodTrackingByIdAndDate( "2023-2-23");
+
   }
+
   macrosChart() {
     const data = {
       labels: [
@@ -25,7 +36,7 @@ export class FoodTrackerComponent implements OnInit{
       ],
       datasets: [{
         label: 'My First Dataset',
-        data: [this.protein*4, this.carbs*4, this.fats*9],
+        data: [this.protein * 4, this.carbs * 4, this.fats * 9],
         backgroundColor: [
           'rgb(200, 0, 200)',
           'rgb(54, 182, 235)',
@@ -50,5 +61,15 @@ export class FoodTrackerComponent implements OnInit{
         }
       })
 
+  }
+
+  getFoodTrackingByIdAndDate( date: String) {
+    this.foodService.getFoodTrackingByIdAndDate( date)
+      .subscribe({
+        next: (data) => {
+          this.foodLogs = data;
+          console.log(data)
+        }
+      })
   }
 }
