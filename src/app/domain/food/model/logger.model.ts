030b03edf68eac66823cc7397c8f8934006224db
Albert <albertcaprar@gmail.com>
2024-03-12 17:33:19 +0200

    added logger model

---
+++ src/app/domain/food/model/logger.model.ts
@@ -0,0 +1,12 @@
+export interface LoggerData{
+  id:number,
+  user_id:number,
+  date:Date,
+  food_id:number,
+  food_name:string,
+  weight:number,
+  calories:number,
+  carbs:number,
+  fats:number,
+  protein:number,
+}
