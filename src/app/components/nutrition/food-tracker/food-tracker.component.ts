bedd0e98768009475166a7bcd8490f5236935dfc
Albert Caprar <albertcaprar@gmail.com>
2024-03-16 02:13:06 +0200

    moved logic in service

--- src/app/components/nutrition/food-tracker/food-tracker.component.ts
+++ src/app/components/nutrition/food-tracker/food-tracker.component.ts
@@ -21,44 +21,8 @@ export class FoodTrackerComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.macrosChart()
+    this.foodService.macrosChart(this.protein, this.carbs, this.fats)
     this.getFoodTrackingByIdAndDate( "2023-2-23");
-
-  }
-
-  macrosChart() {
-    const data = {
-      labels: [
-        'Protein',
-        'Carbs',
-        'Fats'
-      ],
-      datasets: [{
-        label: 'My First Dataset',
-        data: [this.protein * 4, this.carbs * 4, this.fats * 9],
-        backgroundColor: [
-          'rgb(200, 0, 200)',
-          'rgb(54, 182, 235)',
-          'rgb(255, 205, 86)'
-        ],
-        hoverOffset: 4
-      }]
-    };
-
-    new Chart(
-      <HTMLCanvasElement>document.getElementById('macros-chart'),
-      {
-        type: 'doughnut',
-        data: data,
-        options: {
-          cutout: "80%",
-          plugins: {
-            legend: {
-              position: 'right',
-            }
-          }
-        }
-      })
 
   }
 
