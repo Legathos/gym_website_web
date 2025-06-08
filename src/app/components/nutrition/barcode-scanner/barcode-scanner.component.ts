7f290f3a146a37c07d6dce0fa47d00239ca6c6e7
Albert <albertcaprar@gmail.com>
2025-06-08 17:03:06 +0300

    implemented camera live view

--- src/app/components/nutrition/barcode-scanner/barcode-scanner.component.ts
+++ src/app/components/nutrition/barcode-scanner/barcode-scanner.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import { Location } from '@angular/common';
 import { FoodService } from '@domain/food/services/food.service';
 import { FoodData } from '@domain/food';
@@ -11,7 +11,8 @@ import { FoodData } from '@domain/food';
 export class BarcodeScannerComponent implements OnInit {
   // This will be populated when a barcode is scanned
   scannedFood: FoodData | null = null;
-
+  @ViewChild('video', {static: true}) videoElement!: ElementRef<HTMLVideoElement>;
+  stream: MediaStream | null = null;
   // For testing purposes only - remove in production
   mockFoodData: FoodData = {
     id: 1,
@@ -27,10 +28,25 @@ export class BarcodeScannerComponent implements OnInit {
     private location: Location,
   ) {}
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    await this.startCamera();
     // For testing purposes only - remove in production
     // Uncomment the line below to see the food details card
     // this.scannedFood = this.mockFoodData;
+  }
+  ngOnDestroy() {
+    // Clean up the stream
+    if (this.stream) {
+      this.stream.getTracks().forEach(track => track.stop());
+    }
+  }
+  async startCamera() {
+    try {
+      this.stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      this.videoElement.nativeElement.srcObject = this.stream;
+    } catch (err) {
+      console.error('Error accessing camera:', err);
+    }
   }
 
   // Navigate back to the previous page
@@ -48,17 +64,4 @@ export class BarcodeScannerComponent implements OnInit {
     }
   }
 
-  // This method will be called when a barcode is scanned (to be implemented later)
-  onBarcodeScanned(barcode: string): void {
-    // Mock implementation - in the future, this will call an API to get food data by barcode
-    console.log('Barcode scanned:', barcode);
-
-    // For now, we'll just set some mock data for testing
-    this.scannedFood = this.mockFoodData;
-
-    // In a real implementation, we would call the food service to get the data
-    // this.foodService.getFoodByBarcode(barcode).subscribe(food => {
-    //   this.scannedFood = food;
-    // });
-  }
 }
